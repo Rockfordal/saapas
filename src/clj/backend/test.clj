1fc9da7eff23015f3f679474664230eda5af83e5
Anders Lundström <anders@urkraft.se>
2015-10-15 15:25:37 +0200

    förberett för elasticsearch test

--- src/clj/backend/test.clj
+++ src/clj/backend/test.clj
@@ -1,5 +1,10 @@
 (ns backend.test
-  (:require [clojure.test :refer [run-tests]]))
+  (:require [clojure.test :refer [run-tests]]
+            [components.datomic-test]
+            ;[components.elasticsearch-test]
+            ))
 
 (defn run []
-  (run-tests 'components.datomic-test 'components.datomic-test))
+  (run-tests 'components.datomic-test 'components.datomic-test)
+  ;(run-tests 'components.elasticsearch-test 'components.elasticsearch-test)
+  )
