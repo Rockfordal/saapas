d480bbfb901bd15146fcffc0992c9a44735415c3
Anders Lundström <anders@urkraft.se>
2015-10-18 14:04:54 +0200

    httpkit använder webapp och ger den till handlern

--- src/clj/components/httpkit.clj
+++ src/clj/components/httpkit.clj
@@ -8,18 +8,16 @@
   (start [this]
     (let [port (or port 3000)]
       (debugf (str "Starting web server on http://localhost:" port))
-      (assoc this
-             :http-kit
-             (run-server (make-handler nil)
-               {:port port
-                :join? false}))))
+      (assoc this :http-kit
+                  (run-server (make-handler (:webapp this))
+                    {:port port
+                     :join? false}))))
   (stop [this]
     (if-let [http-kit (:http-kit this)]
       (http-kit))
     (assoc this :http-kit nil)))
 
 (defn new-httpkit [opts]
-  ; (using
-  (map->HttpKit opts))
-  ;[:sente])
-  ;[:web-app]
+  (using
+    (map->HttpKit opts)
+    [:webapp]))
